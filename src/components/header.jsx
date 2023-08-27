ea5c51598e339eda71b972f51ce5e17de80f9235
manaim2112 <m.a.naim2112@gmail.com>
2023-08-27 10:33:30 +0700

    new file:   .gitignore 	modified:   README.md 	modified:   index.html 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/vite.svg 	new file:   src/app.css 	new file:   src/assets/preact.svg 	new file:   src/components/header.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   tailwind.config.js 	new file:   vite.config.js

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+export default function Header(prop) {
+    return(
+        <>
+            <div className="sticky top-0 bg-white/30 backdrop-blur-md px-4 md:px-6 lg:px-8 py-4 w-full flex">
+                <div className="font-xl w-2/3">manaim2112</div>
+                <div className="flex-none">
+                    <div>
+                        Home
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
