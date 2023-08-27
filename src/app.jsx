134f60b9fff5cf24ce3634b8664f7f6b8553c7ac
manaim2112 <m.a.naim2112@gmail.com>
2023-08-27 11:26:27 +0700

    modified:   src/app.jsx 	modified:   src/components/header.jsx 	new file:   src/icons/rust.jsx 	new file:   src/icons/tailwind.jsx 	new file:   src/icons/tauri.jsx 	modified:   src/index.css

--- src/app.jsx
+++ src/app.jsx
@@ -3,18 +3,41 @@ import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
 // import './app.css'
 import Header from './components/header'
+import RustIcon from './icons/rust'
+import TailwindIcon from './icons/tailwind'
+import TauriIcon from './icons/tauri'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-gradient-to-br from-white via-yellow-100 to-lime-200'>
+      <div className='bg-gradient-to-br from-gray-200 via-lime-100 to-gray-200'>
         <Header/>
 
 
-          <div className='mt-8'>
-            <div className='font'
+          <div className='mt-8 px-20 pb-20'>
+            <div className='text-4xl'>
+              In My Portofolio
+              <div className='grid grid-cols-2 gap-3 mt-8'>
+                <div>
+                <img className='rounded-lg hover:shadow-lg transition-all hover:scale-110 hover:border-4 hover:border-blue-400' src="https://i.ibb.co/30r4rV8/image.png" alt="image" border="0"/>
+
+                </div>
+                <div>
+                  <div className='text-3xl font-semibold uppercase '>
+                    Alarm Sekolah
+                  </div>
+                  <div className='text-sm flex gap-2 place-items-center'>
+                    <RustIcon className="w-5 h-5"></RustIcon> Rust + <TauriIcon className="h-5"/> + <TailwindIcon className="h-3" />
+                  </div>
+                  <div className='text-sm'>
+                    Free For All
+                  </div>
+                </div>
+
+              </div>
+            </div>
           </div>
       </div>
     </>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,8 @@
 export default function Header(prop) {
     return(
         <>
-            <div className="sticky top-0 bg-white/30 backdrop-blur-md px-4 md:px-6 lg:px-8 py-4 w-full flex">
-                <div className="font-xl w-2/3">manaim2112</div>
+            <div className="sticky top-0 place-items-center bg-white/30 backdrop-blur-md px-4 md:px-6 lg:px-8 py-4 w-full flex">
+                <div className="text-xl w-2/3 font-black">manaim2112</div>
                 <div className="flex-none">
                     <div>
                         Home
